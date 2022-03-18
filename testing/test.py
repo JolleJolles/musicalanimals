624b92cf3fa6ffa47f7318cdbffc8307cf73f065
JolleJolles <j.w.jolles@gmail.com>
2022-03-18 17:24:00 +0100

    updated test file

--- testing/test.py
+++ testing/test.py
@@ -1,3 +1,8 @@
 from manimals import Manimals
 
-Manimals(file = "testing/jolles-stickles-pairs.csv", datacols = ["cx_c"], musicvars = ["pitch"])
+Manimals(file = "testing/jolles-stickles-pairs.csv",
+    datacols = ["cx_c"],
+    musicvars = ["pitch"],
+    timestep=0.1,
+    duration=0.1,
+    datalength=5000)
